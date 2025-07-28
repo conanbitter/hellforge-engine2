b9165595da394efecac14cf4f0805dff7ca590a8
Conan Bitter <conan.bitter@gmail.com>
2025-07-28 20:44:50 +0300

    Add glad library

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,29 @@
 #include <print>
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <glad/gl.h>
 
 int main(int argc, char* argv[]) {
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
         SDL_Log("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
         return 1;
     }
 
-    SDL_Window* window = SDL_CreateWindow("HellForge", 640, 480, SDL_WINDOW_RESIZABLE);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+    SDL_Window* window = SDL_CreateWindow("HellForge", 640, 480, SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL);
     if (window == NULL) {
         SDL_Log("Window could not be created! SDL_Error: %s\n", SDL_GetError());
         SDL_Quit();
         return 1;
     }
+
+    SDL_GLContext context = SDL_GL_CreateContext(window);
+    int version = gladLoadGL((GLADloadfunc)SDL_GL_GetProcAddress);
+    SDL_Log("GL %d.%d\n", GLAD_VERSION_MAJOR(version), GLAD_VERSION_MINOR(version));
 
     bool quit = false;
     SDL_Event e;
@@ -23,8 +33,14 @@ int main(int argc, char* argv[]) {
                 quit = true;
             }
         }
+
+        glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        SDL_GL_SwapWindow(window);
+        SDL_Delay(1);
     }
 
+    SDL_GL_DestroyContext(context);
     SDL_DestroyWindow(window);
     SDL_Quit();
 
