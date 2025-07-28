c461e6f65ba8a7d89aab1f8d32a2235fd0fdd875
Conan Bitter <conan.bitter@gmail.com>
2025-07-28 20:31:32 +0300

    Unwrap minimal SDL app

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,32 @@
 #include <print>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 
-int main() {
-    std::println("Hello {}!", "world");
+int main(int argc, char* argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        SDL_Log("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("HellForge", 640, 480, SDL_WINDOW_RESIZABLE);
+    if (window == NULL) {
+        SDL_Log("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    bool quit = false;
+    SDL_Event e;
+    while (!quit) {
+        while (SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_EVENT_QUIT) {
+                quit = true;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
 }
